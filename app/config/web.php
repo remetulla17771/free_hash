d1d2ec9b53cbf54372c370503d25ec6e750df6a8
remetulla17771 <42893646+remetulla17771@users.noreply.github.com>
2026-08-08 23:40:00 +0500

    refactor: clean component config and expose middleware pipeline

--- app/config/web.php
+++ app/config/web.php
@@ -1,39 +1,42 @@
 <?php
 
+declare(strict_types=1);
 
 return [
     'appName' => 'TinyMVC',
+
     'components' => [
         'user' => [
-            'class' => '\app\AuthService'
+            'class' => '\app\AuthService',
         ],
         'urlManager' => [
-            'class' => '\app\UrlManager'
-        ],
-        'response' => [
-            'class' => '\app\Response'
+            'class' => '\app\UrlManager',
         ],
         'arrayHelper' => [
-            'class' => '\app\helpers\ArrayHelper'
+            'class' => '\app\helpers\ArrayHelper',
         ],
         'language' => [
-            'class' => '\app\helpers\I18n'
+            'class' => '\app\helpers\I18n',
         ],
         'session' => [
-            'class' => '\app\helpers\Session'
+            'class' => '\app\helpers\Session',
         ],
         'stringer' => [
-            'class' => '\app\Request'
+            'class' => '\app\Request',
         ],
     ],
+
+    'middleware' => [],
+
     'modules' => [
         'admin' => [
-            'class' => 'modules\admin\Module',
+            'class' => 'modules\\admin\\Module',
             'layout' => 'admin',
-        ]
+        ],
     ],
+
     'alias' => [
         '@web' => '/',
-        '@uploads' => '/uploads/'
-    ]
+        '@uploads' => '/uploads/',
+    ],
 ];
