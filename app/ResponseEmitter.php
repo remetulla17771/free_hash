35e66d576c71f1284c23b45a1fd8c7feb129d396
remetulla17771 <42893646+remetulla17771@users.noreply.github.com>
2026-08-08 23:33:49 +0500

    refactor: add response emitter

---
+++ app/ResponseEmitter.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app;
+
+final class ResponseEmitter
+{
+    public function emit(Response $response): void
+    {
+        http_response_code($response->getStatusCode());
+
+        foreach ($response->getHeaders() as $name => $value) {
+            header($name . ': ' . $value);
+        }
+
+        echo $response->getContent();
+    }
+}
